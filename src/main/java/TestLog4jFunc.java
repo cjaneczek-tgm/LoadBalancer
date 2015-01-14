f7e502bb04735364322fbfe1f846b958d29f3a5c
Chris <cjaneczek@student.tgm.ac.at>
2015-01-14 16:51:05 +0100

    dvalue

--- src/main/java/TestLog4jFunc.java
+++ src/main/java/TestLog4jFunc.java
@@ -7,7 +7,7 @@ public class TestLog4jFunc {
     static Logger logger = org.apache.log4j.Logger.getLogger(TestLog4jFunc.class);
 
     public static void main(String[] args) {
-        double wtf = 0.47;
-        logger.info("First log-message "+wtf+"%");
+        double dvalue = 0.47;
+        logger.info("First log-message "+dvalue+"%");
     }
 }
