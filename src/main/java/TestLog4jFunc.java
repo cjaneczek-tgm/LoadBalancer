a0efc73cfc38d44c7a92e016935c0a6403beca82
wmair-tgm <wmair@student.tgm.ac.at>
2015-01-14 17:41:39 +0100

    testing git

--- src/main/java/TestLog4jFunc.java
+++ src/main/java/TestLog4jFunc.java
@@ -7,7 +7,7 @@ public class TestLog4jFunc {
     static Logger logger = org.apache.log4j.Logger.getLogger(TestLog4jFunc.class);
 
     public static void main(String[] args) {
-        double dvalue = 0.47;
-        logger.info("First log-message "+dvalue+"%");
+        double dvalue1 = 0.47;
+        logger.info("First log-message "+dvalue1+"%");
     }
 }
