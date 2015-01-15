e0153823e3f0462caaffc6d4b0e2bfcb217f9486
Chris <cjaneczek@student.tgm.ac.at>
2015-01-15 17:49:27 +0100

    LCBalancer Constructor

--- src/main/java/balance/LCBalancer.java
+++ src/main/java/balance/LCBalancer.java
@@ -1,6 +1,14 @@
 package balance;
 
+import balance.bbehavior.BalancerBehavior;
+import balance.bbehavior.LCBehavior;
+
 public class LCBalancer extends Balancer {
+
+    private BalancerBehavior bb;
+    public LCBalancer(BalancerBehavior bb){
+        this.bb = new LCBehavior();
+    }
 
     @Override
     public void run() {
